93ab1618b957220021607cde4a2a0dd1ca251c61
jackcaoyi <jackcaoyi@didichuxing.com>
2019-06-24 11:22:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="margin: 24px">
+    <ButtonCounter />
+    <ButtonJsx />
+  </div>
+</template>
+
+<script>
+import ButtonCounter from './components/ButtonCounter'
+import ButtonJsx from './components/ButtonJsx'
+
+export default {
+  name: 'App',
+  components: {
+    ButtonCounter,
+    ButtonJsx
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/ButtonJsx.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'Button',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {},
+  render(h) {
+    return <div>JSX</div>
+  }
+}
+</script>
+<style>
+</style>
